c8281836255257a86e69d5b94c22ca883dffa7bf
Abhay Mohandas <80393938+abhay-mohandas@users.noreply.github.com>
2022-04-09 22:44:23 +0530

    Update 7z.py

--- 7z.py
+++ 7z.py
@@ -3,48 +3,62 @@ import os
 def clear():
     os.system("clear")
 
-def archive(default=True):
-    if not(default):
-        return
+def archive():
     archive_name=input("Enter the name of the archive[Ex: xyz (or) path/of/the/archive/xyz]:")
     archive_file=input("Enter the file to be compressed(for multiple files, separate with space):")
+    types=["7z","zip","tar","gzip","iso"]
+    print("List of archive formats:")
+    for x in types:
+        print("   ) ."+x)
+    archive_type=input("Enter the compression format(Leave blank to default to .7z):") or "7z"
+    if not(archive_type in types):
+        archive_type = "7z"
     compression_level=input("Enter the compression level(1 to 9, higher number means higher compression):") or "9"
-    os.system("7z -mmt=on -mx="+compression_level+" "+archive_name+" "+archive_file)
+    output_add=input("Enter the output location(Leave blank to place the archive in the current directory):") or ""
+    if output_add:
+        output = "&& mv "+archive_name+" "+output_add
+    else:
+        output = " "
+    os.system("7z a -y -mmt=on -mx="+compression_level+" -t"+archive_type+" "+archive_name+" "+archive_file+" "+output)
 
 
-def delete(default=True):
-    if not(default):
-        return
+def delete():
+    archive_name=input("Enter the name of the archive[Ex: xyz (or) path/of/the/archive/xyz]:")
+    archive_file=input("Optionally enter file names to be deleted from within the archive(Leave space to skip):") or " "
+    output_add=input("Enter the output location(Leave blank to place the archive in the current directory):") or ""
+    if output_add:
+        output = "&& mv "+archive_name+" "+output_add
+    else:
+        output = " "
+    os.system("7z d -y -mmt=on "+archive_name+" "+archive_file+" "+output)
+    
+
+def extract():
+    archive_name=input("Enter the name of the archive[Ex: xyz (or) path/of/the/archive/xyz]:")
+    output_add=input("Enter the output location(Leave blank to place the archive in the current directory):") or ""
+    if output_add:
+        output = "&& mv "+archive_name+" "+output_add
+    else:
+        output = " "
+    os.system("7z e -y -mmt=on "+archive_name+" "+output)
 
 
-def extract(default=True):
-    if not(default):
-        return
+def list_files():
+    archive_name=input("Enter the name of the archive[Ex: xyz (or) path/of/the/archive/xyz]:")
+    os.system("7z l -y "+archive_name)
 
+def test():
+    archive_name=input("Enter the name of the archive[Ex: xyz (or) path/of/the/archive/xyz]:")
+    os.system("7z t -y "+archive_name)
 
-def list_files(default=True):
-    if not(default):
-        return
-
-
-def test(default=True):
-    if not(default):
-        return
-
-def update(default=True):
-    if not(default):
-        return
 
 def main():
     print("[7z archive program]\n")
-    value=False
-    archive_options=   [["1","a","Create a new archive or add files to existing archive",archive(value)],
-                        ["2","d","Delete files from an archive",delete(value)],
-                        ["3","e","Extract files from archive",extract(value)],
-                        ["4","l","List contents of an archive",list_files(value)],
-                        ["5","t","Test archive integrity",test(value)],
-                        ["6","u","Update files in archive",update(value)]]
-    value=True
+    archive_options=   [["1","a","Create a new archive or add files to existing archive",archive],
+                        ["2","d","Delete files from an archive",delete],
+                        ["3","e","Extract files from archive",extract],
+                        ["4","l","List contents of an archive",list_files],
+                        ["5","t","Test archive integrity",test]]
     print("Listing available options:")
     for x in archive_options:
         print("   "+x[0]+")"+x[1]+"("+x[2]+")")
@@ -53,7 +67,8 @@ def main():
         exit()
     for x in archive_options:
         if ans.lower() in x:
-            x[3]
+            clear()
+            x[3]()
             return
     print("\nInvalid option! Try again")
     return main()
